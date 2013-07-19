27f361ba07e8f16ff1860b98f17aa86fbc21190d
Ivan Vendrov <isv452@mail.usask.ca>
2013-07-19 16:30:33 -0600

    using custom wires, implemented a full SIR model with aggregate Vector operations

--- netwire/sir.hs
+++ netwire/sir.hs
@@ -17,7 +17,18 @@ import Data.Monoid
 
 import Control.Wire.Classes
 
-data State = Susceptible | Infectious | Recovered deriving (Eq, Show)
+-- GENERAL NETWORK STUFF (should work both for 2D (DPH) and 1D (GPU) network representations)
+data Network = Network {adjList :: Vector (Vector Int)}
+
+foldAdjList :: Network -> (Vector a -> b) -> Vector a -> Vector b
+-- foldAdjList network agents adjFold returns a vector whose ith element
+-- is adjFold applied to all the neighbours of the ith agent
+foldAdjList network adjFold agents = Vector.map (adjFold . backpermute agents) (adjList network)
+
+
+-- SIR combinators / data 
+
+data State = S | I | R deriving (Eq, Show)
 
 
 -- rate generates events according to a poisson process with parameter 1/mu
@@ -32,27 +43,46 @@ rate mu g
   
 rate' mu = rate mu (mkStdGen 3)
 
-startingInfected = [False, False, False, False]
+-- a wire that takes as input the rate of infection, and produces a unit value with a rate
+-- corresponding to the given time, minus inaccuracy with multiple occurrences in a 
+-- single time interval
+rateWire ::  (RandomGen g) => g -> WireP Double ()
+rateWire g = mkPure $
+           \dt lambda -> 
+               let (e, g') = random g in
+               (if (e < 1 - exp (-dt * lambda)) then Right () else Left mempty, rateWire g')
+
+rateWire' = rateWire (mkStdGen 3)
+               
+
+
 
 
 -- STATECHART specified by a state to wire function,
 -- and a "status" wire that does the plumbing to ensure wires are reset, etc
+infectionRatePerPerson = 0.5
+calcInfectionRate :: Vector State -> Double
+calcInfectionRate = (*infectionRatePerPerson) . 
+                    fromIntegral . 
+                    Vector.length . 
+                    Vector.filter (==I)
 
 --never = Control.Wire.empty -- event that never triggers 
+type Cell = WireP Double State
 
-stateToWire :: State -> WireP () State
+stateToWire :: State -> Cell
 stateToWire state =
     transitions <|> pure state where
         transitions = 
             case state of 
-              Susceptible -> pure Infectious . rate' 2
-              Infectious -> pure Recovered . after 3
-              Recovered -> pure Susceptible . after 3
+              S -> pure I . rateWire' 
+              I -> pure R . after 3
+              R -> pure S . after 3
 
 
 
-status :: WireP () State
-status = rSwitch stateToWire Susceptible
+genCell :: State -> Cell
+genCell = rSwitch stateToWire
 
 -- rSwitch switches between wires based on the given discriminator function "computeWire"
 -- currently, every time its current wire changes output, it plugs the output into computeWire
@@ -71,28 +101,57 @@ rSwitch computeWire initialState = helper' initialState (stateToWire initialStat
                         helper' 
                           newState           
                           (if (newState == state) then currentWire' else stateToWire newState))
+
+-- Parallel wire operator (just pure wires for now)
+par :: Vector (WireP a b) -> WireP (Vector a) (Vector b)
+par wires = 
+    mkPure $ \dt inputs -> 
+        let dts = Vector.replicate (Vector.length wires) dt 
+            (values, wires') = Vector.unzip $ Vector.zipWith3 stepWireP wires dts inputs 
+            right x = case x of 
+                        Right y -> y
+                        Left _ -> error "cannot extract right from a Left contructor"
+            isRight x = case x of
+                          Right _ -> True
+                          Left _ -> False
+            value = if (Vector.all isRight values) 
+                    then Right $ Vector.map right values
+                    else error "not all wires produced" -- TODO fix
+        in (value, par wires')
+ 
            
+-- THIS SPECIFIC EXAMPLE
+sirNetwork = Network $ fromList . List.map (fromList) $ [[1],[0,2],[1,3],[2,4],[3]]
+startingStates = fromList [I, S, S, S, S]
 
-{-
-info :: WireP () Bool
-info = proc _ -> do
-         infected <- delay False . id -< prevInfected
-         if infected
-         then do
-           prevInfected <- id -< infected
-           returnA -< prevInfected
-         else do
-           prevInfected <- arr not -< infected
-           returnA -< prevInfected
-         
-
--}
+sirWire :: WireP (Vector State) (Vector State)
+sirWire = par (Vector.map genCell startingStates) . 
+          arr (foldAdjList sirNetwork calcInfectionRate)
 
 
+--(par cells . arr (foldAdjList sirNetwork calcInfectionRate))
 
+sir, sir' :: WireP () (Vector State)
+sir' = proc _ -> do
+         rec states <- delay startingStates . sirWire -< states
+         returnA -< states
+
+sir = helper startingStates sirWire
+                             where 
+    helper states wire = mkPure $ \dt _ ->                         
+                         let (Right states', wire') = stepWireP wire dt states
+                         in (Right states' , helper states' wire')
+                         
+      
+     
+        
+test :: WireP () Bool
+test = proc _ -> do
+         rec a <- delay True . arr (not) . arr (==False)  -< a
+         returnA -< a
 
 wire :: WireP () String
-wire = forI 100 . arr show . status
+wire = forI 60 . arr show . sir
 
 control whenInhibited whenProduced wire = loop wire (counterSession 0.2) where
     loop w' session' = do
