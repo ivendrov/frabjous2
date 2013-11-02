adc04f10f97a3b39494bb6e9eb0920e6aa810bd9
Ivan Vendrov <isv452@mail.usask.ca>
2013-11-01 18:54:57 -0600

    cleaned up code generator slightly

--- netwire/Compiler.hs
+++ netwire/Compiler.hs
@@ -1,15 +1,31 @@
 {-# LANGUAGE DeriveDataTypeable #-}
 -- The Frabjous Code Generator 
 
+import Debug.Trace
 import Text.ParserCombinators.Parsec
 import qualified Text.Parsec.Token as Token
 import Text.Parsec.Language (haskellDef)
 
-import Data.Maybe (mapMaybe)
+import Data.Maybe (mapMaybe, fromMaybe)
 import Data.List
 import Text.Printf (printf)
 import Data.Data (toConstr, Typeable, Data)
 import Data.Function (on)
+import qualified Language.Haskell.Exts.Parser as Haskell -- needing for working with haskell source code
+import qualified Language.Haskell.Exts.Pretty as Haskell -- needed for working with haskell source code
+
+
+
+
+
+agentKeyword = "agent"
+variableKeyword = "reactive"
+populationKeyword = "population"
+removalKeyword = "removal"
+additionKeyword = "addition"
+networkKeyword = "network"
+keywords = [agentKeyword, variableKeyword, populationKeyword, removalKeyword, networkKeyword] 
+   
 
 
 
@@ -24,11 +40,41 @@ data Dec = AgentDec { agentName :: Name,
                       fields :: [Field]}
          | VariableDec { varName :: Name,
                          code :: HaskellString}
-           deriving (Eq, Ord, Typeable, Data)
+         | PopulationDec { populationName :: Name,
+                           agent :: Name,
+                           removal :: Maybe HaskellString,
+                           addition :: Maybe HaskellString}
+         | NetworkDec { population1 :: (Name, Name),
+                        population2 :: Maybe (Name, Name),
+                        networkSpec :: HaskellString }
+         
+                        
+           deriving (Eq, Typeable, Data)
+
+
+---------------------------------
+---------------------------------
+---------------------------------
+------  UTILITIES
+--------------------------------
+---------------------------------
+---------------------------------
+
+decType :: Dec -> Int
+decType (AgentDec _ _ ) = 0
+decType (VariableDec _ _) = 1
+decType (PopulationDec _ _ _ _) = 2
+decType (NetworkDec _ _ _) = 3
+
+instance Ord Dec where
+    compare = compare `on` decType
+
+type Field = (Name, HaskellString)
 
 name :: Dec -> Name
 name (AgentDec n _) = n
 name (VariableDec n _) = n
+name (PopulationDec n _ _ _ ) = n
 
 
 
@@ -42,9 +88,26 @@ instance Show Dec where
               fields' = fields ++ [("idx" ++ name, ":: Int")]-- add index field
     show (VariableDec name code) = 
         printf "%sWire %s" name code
+    show (PopulationDec name _ _ _ ) = "Population " ++ name
 
-type Field = (Name, HaskellString)
 
+-- converts a haskell declaration to an equivalent one-line declaration 
+-- by inserting braces and semicolons in accordance with the layout rules
+toOneLine :: String -> Maybe String
+toOneLine str = fmap (Haskell.prettyPrintWithMode oneLineMode) decl where
+    decl = case Haskell.parseDecl str of 
+             Haskell.ParseOk decl -> Just decl
+             _ -> Nothing
+    oneLineMode = Haskell.defaultMode {Haskell.layout = Haskell.PPNoLayout}
+
+
+---------------------------------
+---------------------------------
+---------------------------------
+------  CODE GENERATOR
+--------------------------------
+---------------------------------
+---------------------------------
 
 main = do
   contents <- getContents
@@ -60,17 +123,24 @@ process str = case parse program "(unknown)" str of
 generateCode :: Program -> String
 generateCode (Program preamble decs) = 
     let mkLabelsStr = printf "mkLabels [ %s ]\n" (intercalate ", " (map ("''"++) agentNames)) 
-        groupedDecs = groupBy ((==) `on` toConstr) (sort decs)
+        groupedDecs = groupBy ((==) `on` decType) (sort decs)
         agentDecs = groupedDecs !! 0
         variableDecs = groupedDecs !! 1
+        populationDecs = groupedDecs !! 2
+        networkDecs = if (length groupedDecs >= 3) then groupedDecs !! 3 else []
         agentNames = map (name) agentDecs
     in unlines [preamble, 
                 concatMap show agentDecs, 
                 mkLabelsStr,  
                 concatMap show variableDecs,
-                showAgentInstances agentDecs variableDecs]
+                showAgentInstances agentDecs variableDecs,
+                showModelDecs populationDecs,
+                showInitialState populationDecs,
+                showModelEvolution populationDecs networkDecs]
 
 
+-- showAgentInstances prints instance declarations for every agent,
+-- including the code for the agent's local change wire
 -- precondition agentDecs composed of agents, variableDecs composed of variables
 showAgentInstances :: [Dec] -> [Dec] -> String
 showAgentInstances agentDecs variableDecs = 
@@ -103,41 +173,137 @@ showAgentInstances agentDecs variableDecs =
         fieldNames (AgentDec _ fields) = map (fst) fields
         allVarNames = map name variableDecs
 
-        
-                                      
+-- showModelDecs prints declarations for ModelState and ModelOutput
+-- currently just contains the population; later will contain global reactive variables also
+showModelDecs :: [Dec] -> String
+showModelDecs populationDecs = 
+    let populationNames = map (name) populationDecs
+        agentNames = map (\ (PopulationDec _ a _ _) -> a) populationDecs
+        stateFields =  zipWith stateField populationNames agentNames
+        outputFields = zipWith outputField populationNames agentNames
+        stateField, outputField :: Name -> Name -> String
+        stateField pop agent = printf "%sState :: WireP (Vector %s) (PopulationOutput %s)" pop agent agent
+        outputField pop agent = printf "%s :: Vector %s" pop agent
+    in printf "fclabels [d|\n\
+              \    data ModelState = ModelState { %s }\n\
+              \    data ModelOutput = ModelOutput { %s }\n\
+              \          |]\n"
+              (intercalate ", " stateFields)
+              (intercalate ", " outputFields)
+
+-- showInitialState prints the initial model state, using all the populations' specified
+-- removal and addition dynamics
+showInitialState :: [Dec] -> String
+showInitialState populationDecs = 
+    printf "initialModelState :: ModelOutput -> ModelState\n\
+           \initialModelState initialOutput =\n\
+           \   ModelState %s where{ %s }\n" 
+           (intercalate " " $  map (showState) populationDecs)
+           whereDecs 
+        where whereDecs = intercalate " ; " $ map showWires populationDecs
+              showWires :: Dec -> String
+              showWires (PopulationDec name agent removal addition) = 
+                  let removeDec = printf "%sRemove = %s\n" name (fromMaybe "never" removal)
+                      addDec = printf "%sAdd = %s\n" name (fromMaybe "never" addition)
+                  in case (toOneLine removeDec, toOneLine addDec) of 
+                       (Just remove, Just add) -> remove ++ " ; " ++ add
+                       _ -> error "could not parse removal / addition wires"
+              showState :: Dec -> String
+              showState (PopulationDec name agent removal addition) = 
+                   printf "(evolvePopulation (PopulationState (%s) %sRemove %sAdd))" 
+                              (wiresDec :: String)
+                              name
+                              name
+                       where wiresDec = 
+                                 (printf "replicate (length (get %s initialOutput)) localChangeWire" name)
+                   
+-- prints the model evolution wire (handles side effects such as death and network change)                     
+showModelEvolution :: [Dec] -> [Dec] -> String
+showModelEvolution populationDecs networkDecs = 
+    printf "evolveModel :: ModelState -> WireP ModelOutput ModelOutput\n\
+           \evolveModel mstate =\n\
+           \    mkGen $ \\dt input -> do\n%s\
+           \      let\n%s\
+           \      return (Right output, evolveModel (ModelState %s))\n"
+           (concatMap (wireEvolution) popNames)
+           bindings
+           (intercalate " " (map (++ "State") popNames)) 
+        where wireEvolution :: Name -> String
+              wireEvolution n = 
+                  printf "      (Right %sOutput, %sState) <- stepWire (get %sState mstate) dt (get %s input)\n"
+                         n n n n
+              bindings = unlines . map ("        "++) . lines $ (newBindings ++ output)
+              popNames = map (name) populationDecs
+              newBindings = concatMap (newBinding) popNames
+              newBinding name = 
+                  printf "%sNew = %s $ get agents %sOutput\n"
+                         name 
+                         (intercalate " . " . map (processDeath) . mapMaybe (otherPair) $ networkDecs)
+                         name where 
+                             otherPair (NetworkDec (n1,l1) (Just (n2,l2)) _) =
+                                 if (name == n1) then Just (n2,l1) 
+                                 else if (name == n2) then Just (n1,l2)
+                                 else Nothing
+                             otherPair (NetworkDec (n1,l1) Nothing _) = 
+                                 if (name == n1) then Just (n1,l1)
+                                 else Nothing
+                             processDeath (popName, label) = 
+                                 printf "processDeath %s %sNew (get removedIndices %sOutput)" 
+                                        label popName popName
+              output = printf "output = %s $ ModelOutput %s where{ %s }\n" updateNetworks newPops networkWheres
+              updateNetworks, newPops, networkWheres :: String
+              updateNetworks = intercalate " . " (zipWith modifyNetwork networkDecs [0..]) where 
+                  modifyNetwork :: Dec -> Int -> String
+                  modifyNetwork (NetworkDec (n1,l1) Nothing _) n = 
+                      printf "modify %s (computeNetworkSelf %s network%d)" n1 l1 n
+                  modifyNetwork (NetworkDec (n1,l1) (Just (n2,l2)) _) n = 
+                      printf "modify (pairLabel (%s, %s)) (computeNetwork (%s, %s) network%d)" 
+                             n1 n2 l1 l2 n    
+              newPops = intercalate " " . map (++"New") $ popNames
+              networkWheres = intercalate ", " $ zipWith networkWhere networkDecs [0..] where
+                  networkWhere :: Dec -> Int -> String
+                  networkWhere (NetworkDec _ _ code) n =
+                      case toOneLine (printf "network%d = %s" n code) of 
+                        Just result -> result
+                        Nothing -> error "could not parse network declaration"
+                                            
+              
+            
+            
+                                 
 
 
-
-
+---------------------------------
+---------------------------------
+---------------------------------
+------ PARSER
+--------------------------------
+---------------------------------
+---------------------------------
 
 
 program :: GenParser Char st Program
 program = do
-  preamble <- haskellBlock
-  decs <- many dec
-  return (Program preamble decs)
+  preamble <- many haskellBlock
+  decs <- many (skipMany space >> dec)
+  return (Program (unlines preamble) decs)
 
 -- reads a declaration and any trailing whitespace / comments
 dec :: GenParser Char st Dec
 dec = do
-  dec <- choice [agentDec, variableDec]
-  many emptyLine
-  whiteSpace
+  dec <- choice . map try $ [agentDec, variableDec, populationDec, networkDec]
   return dec
 
 
 haskellBlock :: GenParser Char st HaskellString
 haskellBlock = do
-  lines <- many (haskellLine)
-  return (concat lines)
+  fstLine <- haskellLine
+  lines <- many (indentedHaskellLine)
+  return (unlines (fstLine : lines))
 
 
 eol = char '\n'
 
-emptyLine = do
-  whiteSpace
-  eol
-  return ()
 
 
 
@@ -149,15 +315,20 @@ symbol = Token.symbol lexer
 whiteSpace = Token.whiteSpace lexer
 
 haskellLine = do 
-  notFollowedBy (symbol "agent" <|> symbol "reactive")
+  notFollowedBy (choice (map symbol keywords))
   str <- many (noneOf "\n") 
-  nl <- eol
-  return (str ++ [nl])
+  eol
+  return str
+
+indentedHaskellLine = do
+  firstChar <- oneOf " \t"
+  rest <- haskellLine
+  return (firstChar : rest)
 
 
 agentDec :: GenParser Char st Dec
 agentDec = do 
-  symbol "agent"
+  symbol agentKeyword
   id <- identifier
   fields <- braces (commaSep1 field)
   return (AgentDec id fields)
@@ -172,10 +343,46 @@ field = do
 
 variableDec :: GenParser Char st Dec
 variableDec = do 
-  symbol "reactive"
+  symbol variableKeyword
   id <- identifier
   code <- haskellBlock
   return (VariableDec id code)
 
+
+populationDec :: GenParser Char st Dec
+populationDec = do
+  symbol populationKeyword
+  name <- identifier
+  symbol "of"
+  agent <- identifier
+  r <- optionMaybe removalDec
+  a <- optionMaybe additionDec
+  return (PopulationDec name agent r a)
+
+removalDec, additionDec :: GenParser Char st HaskellString
+removalDec = do 
+  symbol removalKeyword
+  symbol "="
+  code <- haskellBlock
+  return code
+
+additionDec = do
+  symbol additionKeyword
+  symbol "="
+  code <- haskellBlock
+  return code
+
+networkDec = do 
+  symbol networkKeyword
+  p1 <- idPair
+  p2 <- optionMaybe (symbol "with" >> idPair)
+  symbol "by"
+  code <- haskellBlock
+  return (NetworkDec p1 p2 code)
   
+
+idPair = do
+  i1 <- identifier
+  i2 <- identifier
+  return (i1, i2)
   
